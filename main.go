aa515eef367e6830cf47e2fc5e63f40d73cd3a4b
foehammer127 <foehammer127@gmail.com>
2023-01-08 22:24:42 -0600

    Add Urfaves Cli
    This will be used as the cli framework for the whole project

--- main.go
+++ main.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	app := &cli.App{
+		Name: "CADBucket",
+		Usage: "Collaborate On Cad Models",
+		Action: func(*cli.Context) error {
+			fmt.Println("Hello World")
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal("err")
+	}
+
 }
 
